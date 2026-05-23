e2750c105066ab5b00f5c74320c47e920bc7e9d3
Constantin Saguin <contact@wolfthemes.com>
2026-05-23 11:48:52 +0200

    dev: rollback npm deployment - Siteground server too limited

---
+++ build/app.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-element'), 'version' => '43beea15e7d5295a7562');
---
+++ build/styles.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'b4c33169d426208429ba');
