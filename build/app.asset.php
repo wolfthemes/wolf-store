b7f550ad0d67450794a9dae017d0db52a1de89dd
Constantin Saguin <contact@wolfthemes.com>
2026-05-23 11:03:23 +0200

    dev: removed build folder

--- build/app.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-element'), 'version' => '29571db6707b0a4741ae');
--- build/styles.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '95898f63ace1310c75b6');
