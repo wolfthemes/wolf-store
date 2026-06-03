a26f1056e492d24bd7943c585b555a6c88e83623
Constantin Saguin <wolfthemes14@gmail.com>
2026-06-03 15:04:20 +0200

    feat: added taxonomy filters

--- src/scripts/components/Sidebar.jsx
+++ src/scripts/components/Sidebar.jsx
@@ -1,75 +1,70 @@
 import { useTerms } from './hooks/useTerms';
 
+const FILTER_GROUPS = [
+    { slug: 'theme_cat',          label: 'Categories' },
+    { slug: 'theme_tag',          label: 'Tags' },
+    { slug: 'theme_color',        label: 'Color' },
+    { slug: 'theme_price',        label: 'Price Range' },
+    { slug: 'theme_style',        label: 'Style' },
+    { slug: 'theme_page_builder', label: 'Page Builder' },
+];
+
+function FilterGroup( { slug, label, activeTaxonomy, activeTermId, onChange } ) {
+    const { terms } = useTerms( slug );
+    if ( ! terms.length ) return null;
+
+    return (
+        <div className='wolf-store-sidebar__group'>
+            <h3 className='wolf-store-sidebar__title'>{ label }</h3>
+            <ul className='wolf-store-sidebar__list'>
+                { terms.map( term => (
+                    <li key={ term.id }>
+                        <a
+                            href='#'
+                            className={ `wolf-store-sidebar__term${
+                                activeTaxonomy === slug && activeTermId === term.id ? ' is-active' : ''
+                            }` }
+                            onClick={ e => { e.preventDefault(); onChange( slug, term.id ); } }
+                        >
+                            { term.term_color && (
+                                <span
+                                    className='wolf-store-sidebar__swatch'
+                                    style={ { background: term.term_color } }
+                                />
+                            ) }
+                            { term.name }
+                            <span className='wolf-store-sidebar__count'>{ term.count }</span>
+                        </a>
+                    </li>
+                ) ) }
+            </ul>
+        </div>
+    );
+}
+
 export default function Sidebar( { activeTaxonomy, activeTermId, onChange } ) {
-    const { terms: cats } = useTerms( 'theme_cat' );
-    const { terms: tags } = useTerms( 'theme_tag' );
-
-    const handleClick = ( e, taxonomy, termId ) => {
-        e.preventDefault();
-        onChange( taxonomy, termId );
-    };
-
     return (
         <aside className='wolf-store-sidebar'>
 
-            { /* All */ }
             <div className='wolf-store-sidebar__group'>
-
-                    <a href='#'
+                <a
+                    href='#'
                     className={ `wolf-store-sidebar__all${ ! activeTermId ? ' is-active' : '' }` }
-                    onClick={ e => handleClick( e, '', 0 ) }
+                    onClick={ e => { e.preventDefault(); onChange( '', 0 ); } }
                 >
                     All Themes
                 </a>
             </div>
 
-            { /* Categories */ }
-            { cats.length > 0 && (
-                <div className='wolf-store-sidebar__group'>
-                    <h3 className='wolf-store-sidebar__title'>Categories</h3>
-                    <ul className='wolf-store-sidebar__list'>
-                        { cats.map( term => (
-                            <li key={ term.id }>
-
-                                    <a href='#'
-                                    className={ `wolf-store-sidebar__term${
-                                        activeTaxonomy === 'theme_cat' && activeTermId === term.id
-                                            ? ' is-active' : ''
-                                    }` }
-                                    onClick={ e => handleClick( e, 'theme_cat', term.id ) }
-                                >
-                                    { term.name }
-                                    <span className='wolf-store-sidebar__count'>{ term.count }</span>
-                                </a>
-                            </li>
-                        ) ) }
-                    </ul>
-                </div>
-            ) }
-
-            { /* Tags */ }
-            {/* { tags.length > 0 && ( */}
-            {/*     <div className='wolf-store-sidebar__group'> */}
-            {/*         <h3 className='wolf-store-sidebar__title'>Tags</h3> */}
-            {/*         <ul className='wolf-store-sidebar__list'> */}
-            {/*             { tags.map( term => ( */}
-            {/*                 <li key={ term.id }> */}
-            {/**/}
-            {/*                         <a href='#' */}
-            {/*                         className={ `wolf-store-sidebar__term${ */}
-            {/*                             activeTaxonomy === 'theme_tag' && activeTermId === term.id */}
-            {/*                                 ? ' is-active' : '' */}
-            {/*                         }` } */}
-            {/*                         onClick={ e => handleClick( e, 'theme_tag', term.id ) } */}
-            {/*                     > */}
-            {/*                         { term.name } */}
-            {/*                         <span className='wolf-store-sidebar__count'>{ term.count }</span> */}
-            {/*                     </a> */}
-            {/*                 </li> */}
-            {/*             ) ) } */}
-            {/*         </ul> */}
-            {/*     </div> */}
-            {/* ) } */}
+            { FILTER_GROUPS.map( group => (
+                <FilterGroup
+                    key={ group.slug }
+                    { ...group }
+                    activeTaxonomy={ activeTaxonomy }
+                    activeTermId={ activeTermId }
+                    onChange={ onChange }
+                />
+            ) ) }
 
         </aside>
     );
