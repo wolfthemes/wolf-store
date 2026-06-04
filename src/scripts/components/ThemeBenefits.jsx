ca2e7b97cc64681385d75e24d7ac4817bc7a8d69
Constantin Saguin <wolfthemes14@gmail.com>
2026-06-04 11:15:52 +0200

    optimized benefits list

--- src/scripts/components/ThemeBenefits.jsx
+++ src/scripts/components/ThemeBenefits.jsx
@@ -4,10 +4,11 @@ export default function ThemeBenefits() {
 		<div className='wolf-theme-benefits'>
 			<h3 className='wolf-theme-benefits__title'>What's Included</h3>
 			<ul className='wolf-theme-benefits__list'>
-				<li>Lifetime theme access</li>
-				<li>Ongoing updates included</li>
+				<li>Direct support from the creator</li>
+				<li>Continuous updates included</li>
 				<li>Direct support from the creator <span className='wolf-theme-benefits__info' title='Support provided via our forum'>ⓘ</span></li>
 				<li>7-day money-back guarantee</li>
+				<li>Trusted by 35,000+ customers</li>
 			</ul>
 		</div>
     );
