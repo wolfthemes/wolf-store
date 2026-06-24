0086a40ec38d11a450e990161ba52e85bdf53339
Constantin Saguin <wolfthemes14@gmail.com>
2026-06-24 16:20:20 +0200

    fix: pass archive pagination settings

--- templates/archive-wolf_theme.php
+++ templates/archive-wolf_theme.php
@@ -7,10 +7,16 @@
 
 defined( 'ABSPATH' ) || exit;
 
-$wolf_store_taxonomy  = '';
-$wolf_store_term      = '';
-$wolf_store_term_id   = 0;
-$wolf_store_term_name = '';
+$wolf_store_taxonomy   = '';
+$wolf_store_term       = '';
+$wolf_store_term_id    = 0;
+$wolf_store_term_name  = '';
+$wolf_store_per_page   = absint( \Wolf_Store\Admin\Options::get_option( 'posts_per_page', 12 ) );
+$wolf_store_pagination = \Wolf_Store\Admin\Options::get_option( 'pagination', 'numbers' );
+
+if ( ! in_array( $wolf_store_pagination, array( 'none', 'numbers', 'load_more' ), true ) ) {
+	$wolf_store_pagination = 'numbers';
+}
 
 if ( is_tax( \Wolf_Store\Config\Taxonomy_Config::get_taxonomy_slugs() ) ) {
 	$wolf_store_queried   = get_queried_object();
@@ -29,7 +35,8 @@ get_header();
 	data-term="<?php echo esc_attr( $wolf_store_term ); ?>"
 	data-term-id="<?php echo absint( $wolf_store_term_id ); ?>"
 	data-term-name="<?php echo esc_attr( $wolf_store_term_name ); ?>"
-	data-pagination="<?php echo esc_attr( \Wolf_Store\Admin\Options::get_option( 'pagination', 'numbers' ) ); ?>">
+	data-per-page="<?php echo absint( $wolf_store_per_page ); ?>"
+	data-pagination="<?php echo esc_attr( $wolf_store_pagination ); ?>">
 </div>
 
 <?php get_footer(); ?>
