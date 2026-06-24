fc9c6da299f1f70ab7e98749001ae8e43eac839c
Constantin Saguin <wolfthemes14@gmail.com>
2026-06-24 10:52:00 +0200

    fix: improve theme grid responsiveness and skeleton

--- src/scripts/components/SkeletonCard.jsx
+++ src/scripts/components/SkeletonCard.jsx
@@ -1,20 +1,19 @@
 export default function SkeletonCard() {
 	return (
 		<article className='wolf-theme-card wolf-theme-card--skeleton'>
-			<div className='wolf-theme-card__thumbnail-link'>
+			<div className='wolf-theme-card__thumbnail-wrap'>
 				<div className='wolf-theme-card__thumbnail wolf-skeleton' />
+				<div className='wolf-theme-card__cat-overlay wolf-skeleton wolf-skeleton--cat' />
 			</div>
 
 			<div className='wolf-theme-card__body'>
 				<div className='wolf-skeleton wolf-skeleton--title' />
-				<div className='wolf-skeleton wolf-skeleton--tagline' />
 			</div>
 
 			<footer className='wolf-theme-card__footer'>
-				<div className='wolf-skeleton wolf-skeleton--price' />
-				<div className='wolf-theme-card__ctas'>
-					<div className='wolf-skeleton wolf-skeleton--btn' />
-					<div className='wolf-skeleton wolf-skeleton--btn' />
+				<div className='wolf-theme-card__price'>
+					<div className='wolf-skeleton wolf-skeleton--price' />
+					<div className='wolf-skeleton wolf-skeleton--price-tagline' />
 				</div>
 			</footer>
 		</article>
